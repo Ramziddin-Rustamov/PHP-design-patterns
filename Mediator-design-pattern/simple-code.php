0b5164291818be3f651bbf5adbfbf89236d8d365
Ramziddin7 <rustamovramziddin7@gmail.com>
2023-11-03 00:17:47 +0100

    Mediator Design pattern

--- Mediator-design-pattern/simple-code.php
+++ Mediator-design-pattern/simple-code.php
@@ -1,36 +1,63 @@
 <?php
 interface ChatMediator {
-    public function sendMessage(string $message ,User $user);
+    public function sendMessage($message ,User $user);
 }
 
 class ChatRoom implements ChatMediator {
     private $users = [];
 
     public function addUser(User $user){
-        $this->user[]= $user;
+        $this->users[]= $user;
     }
 
-    public function sendMessage(string $message, User $user){
-        foreach($this->user as $u){
+    public function sendMessage($message, User $user){
+        foreach($this->users as $u){
             if($u !== $user){
-                $u->sendMessage();
+                $u->recieveMessage($message);
             }
         }
     }
 }
 
 interface User{
-    public function sendMessage(string $message);
-    public function recieveMessage(string $message);
+    public function sendMessage($message);
+    public function recieveMessage($message);
 }
 
 class ChatUser implements User {
       private  $name;
       private $chatMediator;
 
-      public function __construct(string $name , ChatMediator $chatMediator)
+      public function __construct($name , ChatMediator $chatMediator)
       {
         $this->name = $name;
         $this->chatMediator = $chatMediator;
       }
+
+      public function sendMessage($message)
+      {
+        $this->chatMediator->sendMessage($message, $this);
+      }
+
+      public function recieveMessage($message)
+      {
+        echo  $this->name . '  '.'   '."received message !";
+      }
 }
+
+$chatroom = new ChatRoom();
+
+$chatUser1 = new ChatUser("Ramziddin",$chatroom);
+$chatUser2 = new ChatUser("Jonibek",$chatroom);
+$chatUser3 = new ChatUser("Muhriddin",$chatroom);
+
+$chatroom->addUser($chatUser1);
+$chatroom->addUser($chatUser2);
+$chatroom->addUser($chatUser3);
+
+$chatUser1->sendMessage("Hi everyone");
+$chatUser2->sendMessage("Hi Ramziddin");
+
+
+
+
